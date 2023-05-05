dd9cd0296c3a3d74f8d9f3030ee3aee99c984673
LE Dylan <dylan.le@ecole-89.com>
2023-05-05 10:10:29 +0200

    update

--- src/split_inplace.c
+++ src/split_inplace.c
@@ -5,12 +5,16 @@
  * 1st author:  dylan.le - dylan.le
  * description: getline
  */
+#include <stddef.h>
 
 char *split_inplace(char *str, char token)
 {
     int i;
 
     i = 0;
+    if (!str) {
+        return NULL;
+    }
     while(str[i] != token) {
         i += 1;
     }
